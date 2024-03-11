1ae8aea24f849a9a8f6681be77517adea9c4175d
MitraKumar <kaushikmitra556@gmail.com>
2024-03-11 20:15:58 +0530

    Created basic button utility components.

---
+++ src/components/AnimatedLinksWithUnderline.tsx
@@ -0,0 +1,7 @@
+export const AnimatedLinkWithUnderline = ({ text, href, isExternal, isDownloadble }: { text: string, href: string, isExternal?: boolean, isDownloadble?: boolean }) => {
+    return (
+        isDownloadble ?
+            <a className="relative w-fit after:block after:content-[''] after:absolute after:h-[3px] after:bg-gradient-to-br from-cyan-700 to-green-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left text-zinc-100" target={isExternal ? "_blank" : "" } href={href}>{text}</a> :
+            <a className="relative w-fit after:block after:content-[''] after:absolute after:h-[3px] after:bg-gradient-to-br from-cyan-700 to-green-500 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left text-zinc-100" target={isExternal ? "_blank" : "" } href={href}>{text}</a>
+    );
+}
---
+++ src/components/SecondaryButton.tsx
@@ -0,0 +1,11 @@
+import { AnimatedLinkWithUnderline } from "./AnimatedLinksWithUnderline";
+
+export const SecondaryButton = ({ text, href, isExternal, isDownloadble }: { text: string, href: string, isExternal?: boolean, isDownloadble?: boolean }) => {
+    return (
+      <button className="p-1 bg-gradient-to-br from-cyan-700 to-green-500">
+        <span className="py-2 px-5 flex h-full w-full items-center justify-center bg-zinc-900">
+          <AnimatedLinkWithUnderline text={text} href={href} isDownloadble={isDownloadble} isExternal={isExternal} />
+        </span>
+      </button>
+    );
+  }
