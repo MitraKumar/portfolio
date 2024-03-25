665982a56d61a0e07c6c957c5b8e79dca3fc3be2
MitraKumar <kaushikmitra556@gmail.com>
2024-03-25 05:42:29 +0530

    Updated the Projects Data.

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -13,65 +13,19 @@ import {
 import { SecondaryButton } from "@/components/SecondaryButton"
 import { AnimatedLinkWithUnderline } from "./AnimatedLinksWithUnderline";
 
-interface Project {
-  title: string,
-  description: string,
-  url: {
-    github?: string,
-    website?: string,
-  }
-}
-const projects: Project[] = [
-  {
-    title: "phil-o-sophy",
-    description: "This is a simple package which just generates random quotes from Phil Dunphy, the lovable and hilarious character from the TV show Modern Family",
-    url: {
-      github: "https://github.com/MitraKumar/phil-s-osophy",
-    },
-  },
-];
-
-interface Blog {
-  description: string,
-  url: string
-}
-const blogs: Blog[] = [
-  {
-    description: "Use bunx for running npm packages with lightning fast speed.",
-    url: "https://dev.to/mitrakumar/use-bunx-for-running-npm-packages-with-lightning-fast-speed-2p32",
-  },
-  {
-    description: "Journey of creating and publishing a nodejs package with typescript & eslint support",
-    url: "https://dev.to/mitrakumar/journey-of-creating-and-publishing-a-nodejs-package-with-typescript-eslint-support-3gh7",
-  },
-]
+import { getProjects, type Project} from "@/lib/projects";
 
 function ProjectItem(project: Project) {
   return (
-    <CarouselItem>
+    <CarouselItem className="md:basis-1/3">
       <Card>
         <CardContent className="p-4">
           <h2 className="text-4xl mb-4">{project.title}</h2>
           <p>{project.description}</p>
         </CardContent>
-        <CardFooter className="flex justify-end">
-          {project.url.github ? <SecondaryButton href={project.url.github} isExternal label="Github Source Code">Read More</SecondaryButton> : ""}
-          {project.url.website ? <SecondaryButton href={project.url.website} isExternal label="Live Website Link">Read More</SecondaryButton> : ""}
-        </CardFooter>
-      </Card>
-    </CarouselItem>
-  );
-}
-
-function BlogItem(blog: Blog) {
-  return (
-    <CarouselItem>
-      <Card>
-        <CardContent className="p-4">
-          <p>{ blog.description }</p>
-        </CardContent>
-        <CardFooter className="flex justify-end">
-          <AnimatedLinkWithUnderline href={blog.url} isExternal>Check out the Blog...</AnimatedLinkWithUnderline>
+        <CardFooter className="flex justify-end gap-4">
+          {project.links.github ? <SecondaryButton href={project.links.github} isExternal label="Github Source Code">Read More</SecondaryButton> : ""}
+          {project.links.liveUrl ? <SecondaryButton href={project.links.liveUrl} isExternal label="Live Website Link">Visit</SecondaryButton> : ""}
         </CardFooter>
       </Card>
     </CarouselItem>
@@ -80,29 +34,29 @@ function BlogItem(blog: Blog) {
 
 function Projects() {
 
+  const projects = getProjects();
+
   return (
     <div className="py-16 px-4 mt-16">
       <section className="container mx-auto text-white">
         <h2 className="text-6xl">Projects</h2>
-        <div className="mt-8 grid md:grid-cols-2 gap-4">
+        <div className="mt-4">
+          <AnimatedLinkWithUnderline href="/projects">See all Projects...</AnimatedLinkWithUnderline>
+        </div>
+        
+        <div className="mt-8">
           <Carousel>
-            <CarouselContent>
+            <CarouselContent className="mb-8">
               {
                 projects.map(project => <ProjectItem {...project} />)
               }
             </CarouselContent>
+
+            <CarouselPrevious />
+            <CarouselNext />
           </Carousel>
-          <div>
-            <h2 className="text-xl mb-2">Write Ups</h2>
-            <Carousel orientation="vertical">
-              <CarouselContent>
-                {
-                  blogs.map(blog => <BlogItem {...blog} />)
-                }
-              </CarouselContent>
-            </Carousel>
-          </div>
         </div>
+
       </section>
     </div>
   );
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -15,20 +15,32 @@ export const getProjects = function(): Project[] {
   return [
     {
       id: 1,
+      title: "portfolio",
+      subTitle: "This site is a comprehensive showcase of the projects and technologies I've worked with, reflecting my journey and growth as a software developer.",
+      description: "This site is a comprehensive showcase of the projects and technologies I've worked with, reflecting my journey and growth as a software developer.",
+      tech: ["Node", "JavaScript"],
+      image: "/assets/projects/portfolio.png",
+      links: {
+        github: "https://github.com/MitraKumar/portfolio",
+        liveUrl: "/"
+      },
+    },
+    {
+      id: 2,
       title: "phil-o-sophy",
       subTitle: "This is a simple package which just generates random quotes from Phil Dunphy, the lovable and hilarious character from the TV show Modern Family",
       description: "This is a simple package which just generates random quotes from Phil Dunphy, the lovable and hilarious character from the TV show Modern Family",
       tech: ["Node", "JavaScript"],
-      image: "/assets/projects/command-man.png",
+      image: "/assets/projects/phil-o-sophy.png",
       links: {
         github: "https://github.com/MitraKumar/phil-s-osophy",
       },
     },
     {
-      id: 2,
+      id: 3,
       title: "command-man",
-      subTitle: "Used react/redux for creating simple command based game maze solving game.",
-      description: "Used react/redux for creating simple command based game maze solving game.",
+      subTitle: "“Maze Solver” is a command-based game built with React/Redux. It challenges players to solve a maze using terminal-like commands.",
+      description: "“Maze Solver” is a command-based game built with React/Redux. It challenges players to solve a maze using terminal-like commands.",
       tech: ["React", "Redux"],
       image: "/assets/projects/command-man.png",
       links: {
