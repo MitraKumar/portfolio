523bbb873d34c0d7e1760f342782a70407573628
MitraKumar <kaushikmitra556@gmail.com>
2024-03-27 19:58:16 +0530

    Added the blog list with the new write up.

--- src/lib/blogs.ts
+++ src/lib/blogs.ts
@@ -6,11 +6,17 @@ export type Blog = {
 const blogs: Blog[] = [
   {
     id: 1,
+    description:
+      "Using markdown to manage content to NextJS App Router",
+    url: "https://dev.to/mitrakumar/using-markdown-to-manage-content-to-nextjs-app-router-420j",
+  },
+  {
+    id: 2,
     description: "Use bunx for running npm packages with lightning fast speed.",
     url: "https://dev.to/mitrakumar/use-bunx-for-running-npm-packages-with-lightning-fast-speed-2p32",
   },
   {
-    id: 2,
+    id: 3,
     description:
       "Journey of creating and publishing a nodejs package with typescript & eslint support",
     url: "https://dev.to/mitrakumar/journey-of-creating-and-publishing-a-nodejs-package-with-typescript-eslint-support-3gh7",
