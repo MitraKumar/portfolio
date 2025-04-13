e336abbbbc6dac6d561d12b1441c88623f18ff82
MitraKumar <kaushikmitra556@gmail.com>
2025-04-13 20:23:32 +0000

    Fixed the issue for the path for projects.

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -31,7 +31,7 @@ export const getProjects = function (): Project[] {
     },
     {
       id: 2,
-      title: "phil-s-osophy",
+      title: "phil-o-sophy",
       subTitle:
         "This is a simple package which just generates random quotes from Phil Dunphy, the lovable and hilarious character from the TV show Modern Family",
       description:
